ca60ded7b734511dd5a4ef62a68c90ef990fd4a1
Tyler Catus <catus7@yahoo.com>
2023-09-14 14:23:04 -0400

    First Commit

---
+++ controllers/image.js
@@ -0,0 +1,56 @@
+const returnClarifaiRequestOptions = (imageUrl) => {
+
+    const PAT = '73a94a2592a54efe855a8f793fa49c79';
+    const USER_ID = '12345678901';       
+    const APP_ID = 'my-first-application';   
+    const IMAGE_URL = imageUrl;
+  
+    const raw = JSON.stringify({
+      "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL 
+                  }
+              }
+          }
+      ]
+    });
+  
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+      },
+      body: raw
+    };
+    return requestOptions;
+    }
+    
+const handleAPICall = (req, res) => {
+fetch("https://api.clarifai.com/v2/models/face-detection/outputs", returnClarifaiRequestOptions(req.body.input))
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('Unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+   db('users').where('id', '=', id)
+   .increment('entries', 1)
+   .returning('entries')
+   .then(entries => {
+    res.json(entries[0].entries);
+   })
+   .catch(err => res.status(400).json('unable to get entries'))
+   }
+
+   module.exports = {
+    handleImage: handleImage
+   }
