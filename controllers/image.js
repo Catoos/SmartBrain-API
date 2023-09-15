1a0e35f52c99535305eece7c0d6c13b15a4b5af3
Tyler Catus <catus7@yahoo.com>
2023-09-15 12:27:09 -0400

    Readded /imageurl and changed hardcoded port #

--- controllers/image.js
+++ controllers/image.js
@@ -1,39 +1,42 @@
-const returnClarifaiRequestOptions = (imageUrl) => {
+const Clarifai = require('clarifai');
 
-    const PAT = '73a94a2592a54efe855a8f793fa49c79';
-    const USER_ID = '12345678901';       
-    const APP_ID = 'my-first-application';   
-    const IMAGE_URL = imageUrl;
+    // const PAT = '73a94a2592a54efe855a8f793fa49c79';
+    // const USER_ID = '12345678901';       
+    // const APP_ID = 'my-first-application';   
+    // const IMAGE_URL = imageUrl;
   
-    const raw = JSON.stringify({
-      "user_app_id": {
-        "user_id": USER_ID,
-        "app_id": APP_ID
-      },
-      "inputs": [
-          {
-              "data": {
-                  "image": {
-                      "url": IMAGE_URL 
-                  }
-              }
-          }
-      ]
+    // const raw = JSON.stringify({
+    //   "user_app_id": {
+    //     "user_id": USER_ID,
+    //     "app_id": APP_ID
+    //   },
+    //   "inputs": [
+    //       {
+    //           "data": {
+    //               "image": {
+    //                   "url": IMAGE_URL 
+    //               }
+    //           }
+    //       }
+    //   ]
+    // });
+  
+    // const requestOptions = {
+    //   method: 'POST',
+    //   headers: {
+    //       'Accept': 'application/json',
+    //       'Authorization': 'Key ' + PAT
+    //   },
+    //   body: raw
+    // };
+    // return requestOptions;
+    // }
+    const app = new Clarifai.App({
+        apiKey: 'd30e3d69737649da99b8a08138e346c7'
     });
-  
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-          'Accept': 'application/json',
-          'Authorization': 'Key ' + PAT
-      },
-      body: raw
-    };
-    return requestOptions;
-    }
     
 const handleAPICall = (req, res) => {
-fetch("https://api.clarifai.com/v2/models/face-detection/outputs", returnClarifaiRequestOptions(req.body.input))
+app.models.predict('face-detection', req.body.input)
     .then(data => {
         res.json(data);
     })
@@ -52,5 +55,6 @@ const handleImage = (req, res, db) => {
    }
 
    module.exports = {
-    handleImage: handleImage
+    handleImage: handleImage,
+    handleAPICall
    }
